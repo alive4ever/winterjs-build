f30eea704492f8837e7179c4bd74134ab2178e11
alive4ever <alive4ever@users.noreply.github.com>
2025-11-02 05:46:28 +0000

    host: add wget
    Add wget to chroot.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -3,6 +3,6 @@ umask 022
 DEBIAN_CODENAME="trixie"
 sudo apt update
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
-sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,lsb-release,git,python3,python3-venv,python3-distutils-extra,bc,libc6-dev,linux-libc-dev,libelf-dev,binutils-dev,pkgconf "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
+sudo mmdebstrap --arch=arm64 --include sudo,curl,wget,build-essential,devscripts,lsb-release,git,python3,python3-venv,python3-distutils-extra,bc,libc6-dev,linux-libc-dev,libelf-dev,binutils-dev,pkgconf "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
 echo "Container successfully created"
 
