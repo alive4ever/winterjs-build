aea9ba083403d461c6fc0f5ca7ca9cc6e6dbbe08
alive4ever <alive4ever@users.noreply.github.com>
2025-11-02 01:59:37 +0000

    host: add chroot preparation script

---
+++ prepare-host-package.sh
@@ -0,0 +1,8 @@
+set -e
+umask 022
+DEBIAN_CODENAME="trixie"
+sudo apt update
+sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
+sudo mmdebstrap --arch=arm64 --include sudo,curl,build-essential,devscripts,lsb-release,git,python3,python3-venv,python3-distutils-extra,nodejs,bc "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
+echo "Container successfully created"
+
