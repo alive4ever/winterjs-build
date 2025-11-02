6c9c308526b9408ad868327536366b1fd5098732
alive4ever <alive4ever@users.noreply.github.com>
2025-11-02 04:11:58 +0000

    build: switch to main branch
    Switch to main branch and fix some mistakes.

--- build-winterjs.sh
+++ build-winterjs.sh
@@ -8,7 +8,8 @@ export LIBCLANG_PATH="$CLANG_BASE/lib"
 PATH="$CLANG_BASE/bin:$PATH"
 clang --version
 RUST_TARGET="clang -dumpmachine"
-PKG_VERSION="v1.2.0"
+PKG_VERSION="main"
+RELEASE_DIR="/tmp/hosttmp/winterjs_deb"
 curl -L -o rustup-install.sh https://sh.rustup.rs
 sh rustup-install.sh -y --default-toolchain 1.90.0
 . $HOME/.cargo/env
@@ -16,22 +17,23 @@ rustc --version
 cargo --version
 git clone --depth=1 --branch="$PKG_VERSION" https://github.com/wasmerio/winterjs
 cd winterjs
+PKG_COMMIT="$(git log --oneline -1 | cut -d \  -f 1 | head -n 1)"
 cargo build --release
 BIN_PATH=$(find ./target/release -executable -type f -name winterjs | head -n 1)
 echo "Testing the built binary"
 [ -x "$BIN_PATH" ]
 "$BIN_PATH" ./tests/simple.js
-mkdir -p /tmp/hosttmp/winterjs_deb
-PKG_FILENAME="winterjs-$PKG_VERSION-$RUST_TARGET.tar.gz"
-tar --numeric-owner -C ./target/release -cvf - . | gzip -n > /tmp/hosttmp/winterjs_deb/"$PKG_FILENAME"
-cd /tmp/hosttmp/winterjs
+mkdir -p $RELEASE_DIR
+PKG_FILENAME="winterjs-$PKG_VERSION-$PKG_COMMIT-$RUST_TARGET.tar.gz"
+tar --numeric-owner -C ./target/release -cvf - . | gzip -n > $RELEASE_DIR/"$PKG_FILENAME"
+cd $RELEASE_DIR
 sha256sum "$PKG_FILENAME" | tee "${PKG_FILENAME}.sha256"
 cd $OLDPWD
 cargo install cargo-deb
 cargo deb --release
 DEBNAME=$(basename $(find ./target/debian -name '*.deb'))
-cp -v ./target/debian/"$DEBNAME" /tmp/hosttmp/deno_deb/
-cd /tmp/hosttmp/deno_deb
+cp -v ./target/debian/"$DEBNAME" $RELEASE_DIR/
+cd $RELEASE_DIR
 sha256sum "$DEBNAME" | tee "$DEBNAME".sha256
 cd $OLDPWD
 END_TIME=$(date -u +%s.%N)
