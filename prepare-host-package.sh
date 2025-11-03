9de8ef59dce72686761f5ba99ea9dc5443bbcac8
alive4ever <alive4ever@users.noreply.github.com>
2025-11-03 01:45:40 +0000

    host: use deb-src and detailed sources.list

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -3,6 +3,16 @@ umask 022
 DEBIAN_CODENAME="bookworm"
 sudo apt update
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
-sudo mmdebstrap --arch=arm64 --include sudo,curl,wget,build-essential,devscripts,lsb-release,git,bc,libc6-dev,linux-libc-dev,libelf-dev,binutils-dev,pkgconf,sccache,m4 "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
+cat << EOL > /tmp/sources.list
+deb https://deb.debian.org/debian $DEBIAN_CODENAME main non-free-firmware
+deb-src https://deb.debian.org/debian $DEBIAN_CODENAME main non-free-firmware
+
+deb https://security.debian.org/debian-security $DEBIAN_CODENAME-security main non-free-firmware
+deb-src https://security.debian.org/debian-security $DEBIAN_CODENAME-security main non-free-firmware
+
+deb https://deb.debian.org/debian $DEBIAN_CODENAME-updates main non-free-firmware
+deb-src https://deb.debian.org/debian $DEBIAN_CODENAME-updates main non-free-firmware
+EOL
+cat /tmp/sources.list | sudo mmdebstrap --arch=arm64 --include sudo,curl,wget,build-essential,devscripts,lsb-release,git,bc,libc6-dev,linux-libc-dev,libelf-dev,binutils-dev,pkgconf,sccache,m4 "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian https://deb.debian.org/debian
 echo "Container successfully created"
 
