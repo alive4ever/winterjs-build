cbc6ceb6983ab0d8cce2a6f3b141107683276221
alive4ever <alive4ever@users.noreply.github.com>
2025-11-03 01:46:26 +0000

    prepare-build: minor fix

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -2,11 +2,6 @@ set -e
 export DEBIAN_FRONTEND="noninteractive"
 DEBIAN_CODENAME="bookworm"
 echo "deb http://apt.llvm.org/$DEBIAN_CODENAME/ llvm-toolchain-$DEBIAN_CODENAME-20 main" > /etc/apt/sources.list.d/llvm.list
-if grep "deb-src" <  /etc/apt/sources.list ; then
-sed -i".orig" -E "s/#\s+?deb-src/deb-src/" /etc/apt/sources.list
-else
-echo "deb-src http://deb.debian.org/debian/ $DEBIAN_CODENAME main contrib" >> /etc/apt/sources.list
-fi
 curl -sSL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo dd of=/etc/apt/trusted.gpg.d/apt.llvm.org.asc
 apt-get update
 apt-get build-dep -y firefox-esr
