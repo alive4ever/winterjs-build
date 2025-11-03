2116c14cf3d2c5f0b50e3dbe962352ef6ba47a32
alive4ever <alive4ever@users.noreply.github.com>
2025-11-03 01:15:06 +0000

    prepare-build: add deb-src line

--- prepare-build-chroot.sh
+++ prepare-build-chroot.sh
@@ -2,9 +2,10 @@ set -e
 export DEBIAN_FRONTEND="noninteractive"
 DEBIAN_CODENAME="bookworm"
 echo "deb http://apt.llvm.org/$DEBIAN_CODENAME/ llvm-toolchain-$DEBIAN_CODENAME-20 main" > /etc/apt/sources.list.d/llvm.list
+echo "deb-src http://deb.debian.org/debian $DEBIAN_CODENAME main contrib non-free" > /etc/apt/sources.list.d/$DEBIAN_CODENAME-src.list
 curl -sSL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo dd of=/etc/apt/trusted.gpg.d/apt.llvm.org.asc
 apt-get update
-apt-get install -y lld-20 clang-20 clang-tools-20 clang-tidy-20 clang-format-20 libclang-20-dev
 apt-get build-dep -y firefox-esr
+apt-get install -y lld-20 clang-20 clang-tools-20 clang-tidy-20 clang-format-20 libclang-20-dev
 useradd -m -G sudo -s /bin/bash builder
 passwd -d builder
