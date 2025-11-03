991435674a634219dc80ca1dc31a30fae4b1d549
alive4ever <alive4ever@users.noreply.github.com>
2025-11-03 00:43:12 +0000

    host: add m4
    Add m4 to chroot and switch to bookworm.

--- prepare-host-package.sh
+++ prepare-host-package.sh
@@ -1,8 +1,8 @@
 set -e
 umask 022
-DEBIAN_CODENAME="trixie"
+DEBIAN_CODENAME="bookworm"
 sudo apt update
 sudo apt install -y mmdebstrap systemd-container debian-archive-keyring
-sudo mmdebstrap --arch=arm64 --include sudo,curl,wget,build-essential,devscripts,lsb-release,git,bc,libc6-dev,linux-libc-dev,libelf-dev,binutils-dev,pkgconf,sccache "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
+sudo mmdebstrap --arch=arm64 --include sudo,curl,wget,build-essential,devscripts,lsb-release,git,bc,libc6-dev,linux-libc-dev,libelf-dev,binutils-dev,pkgconf,sccache,m4 "$DEBIAN_CODENAME" /var/lib/machines/arm64-debian http://deb.debian.org/debian
 echo "Container successfully created"
 
